657c2a1cafede2caf3a17e2aac72ccd16c16175d
stask <stask@me.com>
2013-08-11 13:10:39 +0300

    added config template; added totals

--- resources/public/js/controllers/instances.js
+++ resources/public/js/controllers/instances.js
@@ -1,30 +1,24 @@
 'use strict';
 
 angular.module('kawsc')
-  .filter('filterByMinPrice', function() {
-    return function(i, minPrice) {
-      return i.kona_price >= $scope.min_price;
-    };
-  })
-  .controller('InstanceListCtrl', function($scope, $http, $log) {
+  .controller('InstanceListCtrl', function($scope, $http, $filter, $log) {
     $scope.loading = true;
     $scope.minPrice = 0;
 
-    $scope.running = function () {
-      return _.filter($scope.instances, function(i) {
+    $scope.visible = function () {
+      return _.filter($filter('filter')($scope.instances, $scope.query), function(i) {
         return i.state.name === "running" && i.kona_price >= $scope.minPrice;
       });
     };
 
+    $scope.total$ = function () {
+      return _.reduce($scope.visible(), function(acc, i) {
+        return acc + i.kona_price;
+      }, 0);
+    };
+
     $http.get('/instances')
       .success(function(data) {
-        //$log.log(data);
-        _.each(data, function(i) {
-          if (i.instance_type === "cc1.4xlarge") {
-            $log.log(i);
-          }
-        });
-
         $scope.instances = data;
         $scope.loading = false;
       }).error(function(data, status) {
